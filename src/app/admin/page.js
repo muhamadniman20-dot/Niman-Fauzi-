586b2130ebc6ed70c9c30b6f549861f13d194e94
Muhamad Niman Fauzi <muhamadniman20@gmail.com>
2026-07-27 22:24:26 +0700

    admin product list

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { supabase } from "../lib/supabase";
 
 export default function Admin() {
@@ -9,22 +9,38 @@ export default function Admin() {
   const [stock, setStock] = useState("");
   const [category, setCategory] = useState("");
   const [affiliateLink, setAffiliateLink] = useState("");
+  const [price, setPrice] = useState("");
+const [editingId, setEditingId] = useState(null);
+const [products, setProducts] = useState([]);
+
+useEffect(() => {
+  loadProducts();
+}, []);
+
+async function loadProducts() {
+  const { data } = await supabase
+    .from("products")
+    .select("*");
+
+  setProducts(data || []);
+}
 async function saveProduct() {
   const { error } = await supabase
     .from("products")
     .insert([
-      {
+  {
   name,
   image,
   stock: Number(stock),
   category,
   affiliate_link: affiliateLink,
+  price: Number(price),
 }
-    ]);
+]);
 
   if (!error) {
     alert("Produk berhasil disimpan!");
-
+loadProducts();
     setName("");
     setImage("");
     setStock("");
@@ -67,6 +83,15 @@ async function saveProduct() {
         />
 
         <input
+  type="number"
+  placeholder="Harga"
+  value={price}
+  onChange={(e) => setPrice(e.target.value)}
+  className="border p-2 w-full"
+/>
+
+
+        <input
           type="text"
           placeholder="Kategori"
           value={category}
@@ -88,6 +113,43 @@ async function saveProduct() {
   Simpan Produk
 </button>
       </form>
-    </main>
+    
+    <hr className="my-6" />
+
+<h2 className="text-2xl font-bold mb-4">
+  Daftar Produk
+</h2>
+<p>Total Produk: {products.length}</p>
+
+<div className="space-y-3">
+  {products.map((product) => (
+    <div
+      key={product.id}
+      className="border p-3 rounded flex justify-between items-center"
+    >
+      <div>
+        <p className="font-bold">{product.name}</p>
+        <p>Rp {product.price}</p>
+     </div>
+
+      <button
+        onClick={() => {
+          setEditingId(product.id);
+        setName(product.name);
+    setImage(product.image);
+    setStock(product.stock);
+    setCategory(product.category);
+    setAffiliateLink(product.affiliate_link);
+    setPrice(product.price);
+  }}
+  className="bg-yellow-500 text-white px-3 py-2 rounded"
+>
+  ✏️ Edit
+</button>
+</div>
+))}
+</div>
+
+</main>
   );
 }
